a5ebe588d0425b61b5b235f70da5aebb21aa70ef
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-10-31 19:35:46 -0700

    feat: spawn rocks and add collision logic

--- test_game/main.py
+++ test_game/main.py
@@ -33,9 +33,17 @@ bucket: List[Union[int, str]] = [bucket_x, bucket_y, BUCKET_WIDTH, BUCKET_HEIGHT
 # money
 MONEY_SIZE: List[int] = [50, 50]
 MONEY_COLOR: str = "#21CE3B"
-FALL_SPEED: int = 3
-money_list: List[List[Union[int, str]]] = []
-SPAWN_RATE: int = 120
+MONEY_FALL_SPEED: int = 3
+MONEY_LIST: List[List[Union[int, str]]] = []
+MONEY_SPAWN_RATE: int = 120
+
+# rock
+ROCK_SIZE: List[int] = [30, 30]
+ROCK_COLOR: str = "#90614c"
+ROCK_FALL_SPEED: int = 10
+ROCK_SPAWN_RATE: int = 500
+ROCK_LIST: List[List[Union[int, str]]] = []
+
 frame_count = 0
 
 score: int = 0
@@ -45,23 +53,44 @@ def update():
 
     # spawn new money
     frame_count += 1
-    if frame_count % SPAWN_RATE == 0:
+    if frame_count % MONEY_SPAWN_RATE == 0:
         rand_x = random.randint(0, SCREEN_WIDTH - MONEY_SIZE[0])
-        money_list.append([rand_x, 0, MONEY_SIZE[0], MONEY_SIZE[1], MONEY_COLOR])
+        MONEY_LIST.append([rand_x, 0, MONEY_SIZE[0], MONEY_SIZE[1], MONEY_COLOR])
+
+    # spawn rock
+    if frame_count % ROCK_SPAWN_RATE == 0:
+        rand_x = random.randint(0, SCREEN_WIDTH - MONEY_SIZE[0])
+        ROCK_LIST.append([rand_x, 0, ROCK_SIZE[0], ROCK_SIZE[1], ROCK_COLOR])
 
     # update money position and check for collisions
     bucket_rect = pygame.Rect(bucket[0], bucket[1], bucket[2], bucket[3])
-    for money in money_list[:]: # slice to allow removal of items
-        money[1] += FALL_SPEED
+    
+    for money in MONEY_LIST[:]: # slice to allow removal of items
+        money[1] += MONEY_FALL_SPEED
 
         money_rect = pygame.Rect(money[0], money[1], money[2], money[3])
         if bucket_rect.colliderect(money_rect):
             # update score
-            score += random.randint(10, 100)
+            score += random.randint(10, 50)
 
-            money_list.remove(money)
+            MONEY_LIST.remove(money)
         elif money[1] > SCREEN_HEIGHT:
-            money_list.remove(money)
+            MONEY_LIST.remove(money)
+
+    # update rock position and check for collisions
+    for rock in ROCK_LIST[:]:
+        rock[1] += ROCK_FALL_SPEED
+
+        rock_rect = pygame.Rect(rock[0], rock[1], rock[2], rock[3])
+        if bucket_rect.colliderect(rock_rect):
+            # play lost animation
+
+            # reset score
+            score = 0
+
+            ROCK_LIST.remove(rock)
+        elif rock[1] > SCREEN_HEIGHT:
+            ROCK_LIST.remove(rock)
 
     
 
@@ -84,8 +113,12 @@ def render():
     screen.fill(pygame.Color(BACKGROUND_COLOR))
 
     # render money
-    for money in money_list:
+    for money in MONEY_LIST:
         pygame.draw.rect(screen, pygame.Color(money[4]), money[:4])
+
+    # render rocks
+    for rock in ROCK_LIST:
+        pygame.draw.rect(screen, pygame.Color(rock[4]), rock[:4])
 
     # render bucket
     pygame.draw.rect(screen, pygame.Color(bucket[4]), bucket[:4])
