00b0222314d558a52995b149b29664d03a64f7e9
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-11-27 08:56:05 -0800

    feat: create player class and add render logic

---
+++ brick_breaker/entities/player.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Player:
+    def __init__(self, x: int, y: int, width: int = 10, height: int = 5, speed: int = 3):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.color = pygame.Color("#ffffff")
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def update(self):
+        pass
+
+    def render(self, screen: pygame.Surface):
+        pygame.draw.rect(screen, self.color, self.hitbox)
