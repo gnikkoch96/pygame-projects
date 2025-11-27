aeeb0d118b6059a1f6177c3c4b88cb784374fa65
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-11-27 09:08:33 -0800

    feat: add handle input logic

--- brick_breaker/entities/player.py
+++ brick_breaker/entities/player.py
@@ -1,4 +1,6 @@
 import pygame
+from typing import Sequence
+from config import SCREEN_WIDTH
 
 class Player:
     def __init__(self, x: int, y: int, width: int = 100, height: int = 10, speed: int = 3):
@@ -10,7 +12,20 @@ class Player:
         self.color = pygame.Color("#ffffff")
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
+    def handle_input(self, keys: Sequence[bool]):
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.x >= 0:
+            self.x -= self.speed
+
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.x + self.width <= SCREEN_WIDTH:
+            self.x += self.speed
+
+        # let go of ball and/or speed up ball 
+        if keys[pygame.K_SPACE]:
+            pass
+
+    
     def update(self):
+        # update hitbox 
         pass
 
     def render(self, screen: pygame.Surface):
