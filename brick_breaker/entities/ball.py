a1dd72cbb78d43503e218032cad5f28188ff955c
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-11-27 08:37:07 -0800

    update: ball class to handle update and render logic

--- brick_breaker/entities/ball.py
+++ brick_breaker/entities/ball.py
@@ -1,9 +1,26 @@
+import pygame
+from config import SCREEN_HEIGHT, SCREEN_WIDTH
+
 class Ball:
-    def __init__(self):
-        pass
+    def __init__(self, x: int, y: int, size: int = 2, speed_x: int = 2, speed_y: int = 2):
+        self.x = x
+        self.y = y
+        self.size = size # diameter
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        self.color = pygame.Color("#ffffff")
 
     def update(self):
-        pass
+        # bounce x-axis
+        if self.x + self.size  >= SCREEN_WIDTH:
+            self.speed_x *= -1 
 
-    def render(self):
-        pass
+        # bounce y-axis
+        if self.y + self.size >= SCREEN_HEIGHT:
+            self.speed_y *= -1 
+    
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+    def render(self, screen: pygame.Surface):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
