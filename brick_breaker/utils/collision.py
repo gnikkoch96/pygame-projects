4a44ba542b92c2d0bb39b7a6ed84b43ee32add41
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-11-29 21:48:54 -0800

    fix: make ball collision more accurate based on the left and right side of the ball

--- brick_breaker/utils/collision.py
+++ brick_breaker/utils/collision.py
@@ -5,10 +5,10 @@ def handle_collision(ball: Ball, player: Player, brick):
     # ball -> paddle collision
     if ball.hitbox.colliderect(player.hitbox):
         # determine which part of the player the ball collided with
-        if ball.hitbox.x < player.hitbox.x + player.hitbox.width // 3:
+        if ball.hitbox.x - ball.radius  < player.hitbox.x + player.hitbox.width // 3:
             # left
             ball.speed_x = -abs(ball.speed_x)
-        elif ball.hitbox.x > player.hitbox.x + (2 * player.hitbox.width // 3):
+        elif ball.hitbox.x + ball.radius > player.hitbox.x + (2 * player.hitbox.width // 3):
             # right
             ball.speed_x = abs(ball.speed_x)
 
