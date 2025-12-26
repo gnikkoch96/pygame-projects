6854dba3d8389c75dc3655d171c77d53649ef494
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-12-25 20:15:20 -0800

    feat: update next level check where the length of bricks should be equal to 0 or less

--- brick_breaker/main.py
+++ brick_breaker/main.py
@@ -84,8 +84,8 @@ def render():
 
     if game_state.player.lives <= 0:
         show_dialog(screen, game_state, dialog_font, button_font, True)
-        
-    if len(game_state.bricks) <= 12:
+
+    if len(game_state.bricks) <= 0:
         show_dialog(screen, game_state, dialog_font, button_font, False, True)
 
 while game_state.running:
